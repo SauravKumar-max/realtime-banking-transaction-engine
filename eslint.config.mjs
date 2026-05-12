308ebdbea738108befed965b14e5b42cd3fc96ce
SauravKumar-max <maxsaurav237@gmail.com>
2026-05-12 17:51:56 +0530

    - added argon2 for hashing password, connect-pg-simple and express-session for session management

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import { defineConfig } from 'eslint/config';
+
+export default defineConfig([
+  {
+    ignores: ['coverage/**', 'dist/**', 'drizzle/meta/**', 'node_modules/**'],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintConfigPrettier,
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: globals.node,
+    },
+  },
+]);
--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,9 +1,9 @@
 import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/node-postgres';
 
-export const db = drizzle({ 
-  connection: { 
+export const db = drizzle({
+  connection: {
     connectionString: process.env.DATABASE_URL!,
     ssl: process.env.DATABASE_SSL === 'true',
-  }
+  },
 });
