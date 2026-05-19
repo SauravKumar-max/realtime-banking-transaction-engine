363879bfea55a1f11f50b2095e80590ef3a2985f
SauravKumar-max <maxsaurav237@gmail.com>
2026-05-19 15:03:34 +0530

    - fix: added next parameter to middleware

--- src/middlewares/error-handler.middleware.ts
+++ src/middlewares/error-handler.middleware.ts
@@ -1,7 +1,8 @@
-import type { Request, Response } from 'express';
+import type { Request, Response, NextFunction } from 'express';
 import type { AppError } from '../types/error.types.js';
 
-export function errorHandler(error: AppError, req: Request, res: Response) {
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export function errorHandler(error: AppError, req: Request, res: Response, _next: NextFunction) {
   if (error?.statusCode) {
     return res.status(error.statusCode).json({
       message: error.message,
